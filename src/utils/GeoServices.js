367a5ca2a6e921cf21afc15848dcd775d69d1e71
datajonas82 <121538938+datajonas82@users.noreply.github.com>
2025-08-14 08:53:38 +0200

    Endret 'kjøre' til 'kjør' i forslaget om starttid

--- src/utils/GeoServices.js
+++ src/utils/GeoServices.js
@@ -469,7 +469,7 @@ const formatWaitTime = (waitMinutes, allDepartures = [], drivingTime = 0) => {
     }
     
     if (suggestedDepartureTime) {
-      return `<span style="color: #f59e0b; font-weight: bold;">Du må vente i ${waitTimeText} til neste avgang. <span style="color: #000000; font-weight: bold;">Start å kjøre kl. ${suggestedDepartureTime} for å rekke fergen med 5 minutter margin.</span></span>`;
+      return `<span style="color: #f59e0b; font-weight: bold;">Du må vente i ${waitTimeText} til neste avgang. <span style="color: #000000; font-weight: bold;">Start å kjør kl. ${suggestedDepartureTime} for å rekke fergen med 5 minutter margin.</span></span>`;
     } else {
       return `<span style="color: #f59e0b; font-weight: bold;">Du må vente i ${waitTimeText} til neste avgang</span>`;
     }
